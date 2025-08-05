c0cc14bc5a0e9620eaf7d44d3d4fcc98dbae7de6
Tomasz Herman <tomekh2015@gmail.com>
2025-08-05 14:10:48 +0200

    Fixed issues

--- app/main.py
+++ app/main.py
@@ -63,10 +63,10 @@ class DeliveryDrone(FlyingRobot):
     ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
-        self.current_load = None
+        self.current_load = None  # default value
 
         if current_load is not None:
-            self.hook_load(current_load)
+            self.hook_load(current_load)  # don't override this later
 
     def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and cargo.weight <= self.max_load_weight:
